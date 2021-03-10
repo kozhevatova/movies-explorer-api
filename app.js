2e2a91f3ac4a5d36b8ffdbf514f6b6140d2da92d
Anna Kozhevatova <ad.kozhevatova@yandex.ru>
2021-03-10 16:45:24 +0300

    feat: подготовка проекта

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config({ path: './env' });
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+// подключение к серверу mongo
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const options = {
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'authorization'],
+  credentials: true,
+};
+app.use('*', cors(options));
+
+app.use(helmet());
+//bodyParser deprecated
+app.use(express.json());
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Application is running in port ${PORT}`);
+});
